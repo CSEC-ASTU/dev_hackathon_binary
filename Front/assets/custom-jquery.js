e89a2a78d7b8ce1d16e655f6659726887743cf4e
Mohammed Ali <mohaali482@gmail.com>
2022-05-07 00:19:05 +0300

    corrected some bug

--- Front/assets/custom-jquery.js
+++ Front/assets/custom-jquery.js
@@ -1,13 +1,13 @@
 function hoverDiv(e,div){
 
     const popup = $(div).parent().next()
-    popup.removeClass("d-none")
+    popup.addClass("d-md-block")
     return false;
 }
 
 function mouseLeave(e,div){
 
     const popup = $(div).parent().next()
-    popup.addClass("d-none");
+    popup.removeClass("d-md-block");
     return false;
 }
