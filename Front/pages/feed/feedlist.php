4725e45595e7baa6cf97fc37264501b98e217a2d
Mohammed Ali <mohaali482@gmail.com>
2022-05-07 17:35:56 +0300

    changed the feedlist.php file

--- Front/pages/feed/feedlist.php
+++ Front/pages/feed/feedlist.php
@@ -9,27 +9,40 @@
         </div>
     </div>
 
-    <div class="card" style="width: fit-content;">
-        <div class="card-header d-flex align-items-center">
-            <img src="assets/dist/img/logo.jpg" class="post-image">
-            <p class="post-poster" onmouseover="hoverDiv(event,this)" onmouseleave="mouseLeave(event, this)">Name Here</p>
-            <p class="post-time text-light">6 Jul</p>
+    <!-- Post -->
+    <div class="post" style="position: relative">
+        <div class="user-block">
+            <img class="img-circle img-bordered-sm" src="../../dist/img/user6-128x128.jpg" alt="User Image" />
+            <span class="username">
+                <a href="#" onmouseover="hoverDiv(this)" onmouseleave="mouseLeave(this)">Adam Jones</a>
+            </span>
+            <span class="description">Posted 5 photos - 5 days ago</span>
         </div>
 
         <div class="popup-account d-none">
             <div class="card">
                 <div class="card-header">
-                    <img src="assets/dist/img/logo.jpg" class="post-image">
-                    <p style="font-weight: bold;">Name Here</p>
-                    <p style="font-weight: lighter;">description about the "Name Here"</p>
+                    <img src="assets/dist/img/logo.jpg" class="post-image" />
+                    <p style="font-weight: bold">Name Here</p>
+                    <p style="font-weight: lighter">
+                        description about the "Name Here"
+                    </p>
 
-                    <p style="margin-top: 30px; font-weight: bold;">100 Members</p>
+                    <p style="margin-top: 30px; font-weight: bold">
+                        100 Members
+                    </p>
                 </div>
             </div>
         </div>
-
-        <div class="card-body">
-            <p class="post-post">If Twitter acquisition completes, company will be super focused on hardcore software engineering, design, infosec & server hardware</p>
+        <!-- /.user-block -->
+        <div class="row mb-3">
+            <div class="col-sm-6">
+                <img class="img-fluid mb-3" src="assets/dist/img/logo.jpg" alt="Photo" />
+            </div>
+            <!-- /.col -->
+            <div class="col-sm-6">
+                <img class="img-fluid mb-3" src="assets/dist/img/logo.jpg" alt="Photo" />
+            </div>
 
             <div class="card d-flex flex-column align-items-center">
                 <div class="card-header w-75 d-flex justify-content-center">
@@ -45,43 +58,57 @@
                     <p class="">Log into Facebook to start sharing and connecting with your friends, family, and people you know.</p>
                 </div>
             </div>
+            <!-- /.col -->
         </div>
-        <div class="card-footer">
-            <div class="d-flex justify-content-around">
-                <div class="like d-flex align-items-center">
-                    <i class="far fa-heart"></i>
-                    <p>10</p>
-                </div>
+        <!-- /.row -->
 
-                <div class="comment d-flex align-items-center">
-                    <i class="far fa-comment"></i>
-                    <p>10</p>
-                </div>
-            </div>
-        </div>
+        <p>
+            <a href="#" class="link-black text-sm mr-2"><i class="fas fa-share mr-1"></i> Share</a>
+            <a href="#" class="link-black text-sm"><i class="far fa-thumbs-up mr-1"></i> Like</a>
+            <span class="float-right">
+                <a href="#" class="link-black text-sm">
+                    <i class="far fa-comments mr-1"></i> Comments (5)
+                </a>
+            </span>
+        </p>
+
+        <input class="form-control form-control-sm" type="text" placeholder="Type a comment" />
     </div>
 
-    <div class="card" style="width: fit-content;">
-        <div class="card-header d-flex align-items-center">
-            <img src="assets/dist/img/logo.jpg" class="post-image">
-            <p class="post-poster" onmouseover="hoverDiv(event,this)" onmouseleave="mouseLeave(event, this)">Name Here</p>
-            <p class="post-time text-light">6 Jul</p>
+
+    <div class="post" style="position: relative">
+        <div class="user-block">
+            <img class="img-circle img-bordered-sm" src="../../dist/img/user6-128x128.jpg" alt="User Image" />
+            <span class="username">
+                <a href="#" onmouseover="hoverDiv(this)" onmouseleave="mouseLeave(this)">Adam Jones</a>
+            </span>
+            <span class="description">Posted 5 photos - 5 days ago</span>
         </div>
 
         <div class="popup-account d-none">
             <div class="card">
                 <div class="card-header">
-                    <img src="assets/dist/img/logo.jpg" class="post-image">
-                    <p style="font-weight: bold;">Name Here</p>
-                    <p style="font-weight: lighter;">description about the "Name Here"</p>
+                    <img src="assets/dist/img/logo.jpg" class="post-image" />
+                    <p style="font-weight: bold">Name Here</p>
+                    <p style="font-weight: lighter">
+                        description about the "Name Here"
+                    </p>
 
-                    <p style="margin-top: 30px; font-weight: bold;">100 Members</p>
+                    <p style="margin-top: 30px; font-weight: bold">
+                        100 Members
+                    </p>
                 </div>
             </div>
         </div>
-
-        <div class="card-body">
-            <p class="post-post">If Twitter acquisition completes, company will be super focused on hardcore software engineering, design, infosec & server hardware</p>
+        <!-- /.user-block -->
+        <div class="row mb-3">
+            <div class="col-sm-6">
+                <img class="img-fluid mb-3" src="assets/dist/img/logo.jpg" alt="Photo" />
+            </div>
+            <!-- /.col -->
+            <div class="col-sm-6">
+                <img class="img-fluid mb-3" src="assets/dist/img/logo.jpg" alt="Photo" />
+            </div>
 
             <div class="card d-flex flex-column align-items-center">
                 <div class="card-header w-75 d-flex justify-content-center">
@@ -97,72 +124,21 @@
                     <p class="">Log into Facebook to start sharing and connecting with your friends, family, and people you know.</p>
                 </div>
             </div>
+            <!-- /.col -->
         </div>
-        <div class="card-footer">
-            <div class="d-flex justify-content-around">
-                <div class="like d-flex align-items-center">
-                    <i class="far fa-heart"></i>
-                    <p>10</p>
-                </div>
+        <!-- /.row -->
 
-                <div class="comment d-flex align-items-center">
-                    <i class="far fa-comment"></i>
-                    <p>10</p>
-                </div>
-            </div>
-        </div>
-    </div>
+        <p>
+            <a href="#" class="link-black text-sm mr-2"><i class="fas fa-share mr-1"></i> Share</a>
+            <a href="#" class="link-black text-sm"><i class="far fa-thumbs-up mr-1"></i> Like</a>
+            <span class="float-right">
+                <a href="#" class="link-black text-sm">
+                    <i class="far fa-comments mr-1"></i> Comments (5)
+                </a>
+            </span>
+        </p>
 
-    <div class="card" style="width: fit-content;">
-        <div class="card-header d-flex align-items-center">
-            <img src="assets/dist/img/logo.jpg" class="post-image">
-            <p class="post-poster" onmouseover="hoverDiv(this)" onmouseleave="mouseLeave(this)">Name Here</p>
-            <p class="post-time text-light">6 Jul</p>
-        </div>
-
-        <div class="popup-account d-none">
-            <div class="card">
-                <div class="card-header">
-                    <img src="assets/dist/img/logo.jpg" class="post-image">
-                    <p style="font-weight: bold;">Name Here</p>
-                    <p style="font-weight: lighter;">description about the "Name Here"</p>
-
-                    <p style="margin-top: 30px; font-weight: bold;">100 Members</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-body">
-            <p class="post-post">If Twitter acquisition completes, company will be super focused on hardcore software engineering, design, infosec & server hardware</p>
-
-            <div class="card d-flex flex-column align-items-center">
-                <div class="card-header w-75 d-flex justify-content-center">
-                    <!-- image is found on the meta of the page -->
-                    <img src="https://www.facebook.com/images/fb_icon_325x325.png" style="max-width: 540px !important;">
-                </div>
-                <div class="card-footer">
-                    <!-- url of the page -->
-                    <p class="post-link text-light">facebook.com</p>
-                    <!-- site name of the page -->
-                    <p class="text-bold">Facebook</p>
-                    <!-- description of the page. -->
-                    <p class="">Log into Facebook to start sharing and connecting with your friends, family, and people you know.</p>
-                </div>
-            </div>
-        </div>
-        <div class="card-footer">
-            <div class="d-flex justify-content-around">
-                <div class="like d-flex align-items-center">
-                    <i class="far fa-heart"></i>
-                    <p>10</p>
-                </div>
-
-                <div class="comment d-flex align-items-center">
-                    <i class="far fa-comment"></i>
-                    <p>10</p>
-                </div>
-            </div>
-        </div>
+        <input class="form-control form-control-sm" type="text" placeholder="Type a comment" />
     </div>
 
 </div>
